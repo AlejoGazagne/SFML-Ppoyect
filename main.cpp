0369593b96902b7ccd94bc3dcf4ac1dc2855de76
Alejo Gazagne <agazagne026@alumnos.iua.edu.ar>
2021-09-30 19:51:24 -0300

    Prueba segundo personaje

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include "Clases/personaje1.h"
 
 int main() {
-
+    //Crear ventana y mostrar el mapa
     sf::RenderWindow window(sf::VideoMode(1536, 990), "Proyecto Info II");
     window.setFramerateLimit(60);
     sf::Texture texture1;
@@ -17,6 +17,7 @@ int main() {
     image1.setTexture(texture1);
     image1.setPosition(0, 0);
 
+    //Creo el primer personaje
     sf::Texture texture;
     sf::Sprite image;
     if (!texture.loadFromFile("../assets/PJ.png")) {
@@ -24,9 +25,19 @@ int main() {
         return EXIT_FAILURE;
     }
     image.setTexture(texture);
-    //image.setPosition(-10, 0);
 
     Personaje1 player1(200, 680, texture);
+
+    //Creo el segundo personaje
+    sf::Texture texture2;
+    sf::Sprite image2;
+    if (!texture2.loadFromFile("../assets/PJ2.png")) {
+        std::cout << "Error al cargar textura, verifique la ruta";
+        return EXIT_FAILURE;
+    }
+    image2.setTexture(texture2);
+
+    Personaje1 player2(1000, 680, texture2);
 
     // Main loop
     while (window.isOpen()) {
@@ -38,18 +49,26 @@ int main() {
         }
         // Update world parameters
 
-        // player.move(3,4);
+        // player1.move(3,4);
         if(sf::Keyboard::isKeyPressed(sf::Keyboard::D)){
             player1.moverDerecha();
         }
         if(sf::Keyboard::isKeyPressed(sf::Keyboard::A)){
             player1.moverIzquierda();
         }
+        //Mover player 2
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::L)){
+            player2.moverDerecha();
+        }
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::J)){
+            player2.moverIzquierda();
+        }
 
         // Draw all elements
         window.clear();
         window.draw(image1);
         player1.dibujar(window);
+        player2.dibujar(window);
         window.display();
 
     }
